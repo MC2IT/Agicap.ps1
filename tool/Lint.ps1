8f9f8293d1beb0855e293c0293e98e1d82ea2dbc
Cédric Belin <cedx@outlook.com>
2026-04-08 21:51:28 +0200

    Fix the build system

--- tool/Lint.ps1
+++ tool/Lint.ps1
@@ -1,4 +1,4 @@
 "Performing the static analysis of source code..."
 Import-Module PSScriptAnalyzer
 $PSScriptRoot, "src", "test" | Invoke-ScriptAnalyzer -Recurse
-Test-ModuleManifest SetupAnt.psd1 | Out-Null
+Test-ModuleManifest Agicap.psd1 | Out-Null
