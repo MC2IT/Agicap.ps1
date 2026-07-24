f8f4b24faa45cc343a2392e9fdde715b2e2e83b1
Cédric Belin <cedx@outlook.com>
2026-07-24 17:46:11 +0200

    Add the `ThirdParty` class

--- Sources/ChartOfAccounts/AccountingAccountType.cs
+++ Sources/ChartOfAccounts/AccountingAccountType.cs
@@ -1,11 +1,8 @@
-using System.Text.Json.Serialization;
-
 namespace Mc2it.Agicap.ChartOfAccounts;
 
 /// <summary>
 /// Defines the type of an accounting account.
 /// </summary>
-[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum AccountingAccountType {
 	Bank,
 	Client,
