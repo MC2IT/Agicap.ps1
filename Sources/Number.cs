cd7abf08ac5c25cd56f806907c417bac44d22ea5
Cédric Belin <cedx@outlook.com>
2026-07-25 12:28:32 +0200

    Code optimization [skip ci]

--- Sources/Number.cs
+++ Sources/Number.cs
@@ -11,10 +11,14 @@ public static class Number {
 	/// <param name="value">The value to convert.</param>
 	/// <returns>A 32-bit signed integer that is equivalent to the specified value, or <see langword="null"/> if the conversion is not supported.</returns>
 	public static int? AsInt32(object? value) => value switch {
+		sbyte int8 => int8,
 		byte uint8 => uint8,
 		short int16 => int16,
+		ushort uint16 => uint16,
 		int int32 => int32,
+		uint uint32 => uint32 <= int.MaxValue ? (int) uint32 : null,
 		long int64 => int64 >= int.MinValue && int64 <= int.MaxValue ? (int) int64 : null,
+		ulong uint64 => uint64 <= int.MaxValue ? (int) uint64 : null,
 		_ => null
 	};
 }
