1f1ac50baa0fa2d9078766dfe073d7967cecf09f
Cédric Belin <cedx@outlook.com>
2026-07-25 12:35:07 +0200

    Add the `PurchaseJournalTypology` enum

---
+++ Sources/PurchaseJournal/PurchaseJournalTypology.cs
@@ -0,0 +1,12 @@
+namespace Mc2it.Agicap.PurchaseJournal;
+
+/// <summary>
+/// Defines the source document type of a purchase journal.
+/// </summary>
+public enum PurchaseJournalTypology {
+	CardExpenseReceipt,
+	CardRefundReceipt,
+	CreditNote,
+	ExpenseClaim,
+	OwedInvoice
+}
