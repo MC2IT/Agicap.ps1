83415e54a37b1c672f286004a85a0a15a27521b7
Cédric Belin <cedx@outlook.com>
2026-07-25 12:35:23 +0200

    Add the `AccountingLineAccountType` enum

---
+++ Sources/PurchaseJournal/AccountingLineAccountType.cs
@@ -0,0 +1,10 @@
+namespace Mc2it.Agicap.PurchaseJournal;
+
+/// <summary>
+/// Defines the account type of an accounting line
+/// </summary>
+enum AccountingLineAccountType {
+	ExpenseAccount,
+	SupplierAccount,
+	VatAccount
+}
