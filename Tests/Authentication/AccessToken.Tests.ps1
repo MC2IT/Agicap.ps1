b81a430e43a80d1a9c8dae81595e1529aeaabedd
Cédric Belin <cedx@outlook.com>
2026-07-26 13:50:17 +0200

    Update the tests

--- Tests/Authentication/AccessToken.Tests.ps1
+++ Tests/Authentication/AccessToken.Tests.ps1
@@ -8,13 +8,7 @@ using module ../../Agicap.psd1
 Describe "AccessToken" {
 	Context "FromJson" {
 		It "should create an access token from the specified JSON payload" {
-			$token = [Mc2it.Agicap.Authentication.AccessToken] (ConvertFrom-Json '{
-				"access_token": "a1704b4b-7662-432e-a68e-77f414fb836c",
-				"expires_in": 3600,
-				"scope": "agicap:public-api public-api:import_payment_files public-api:manage-payment-beneficiaries",
-				"token_type": "OAuth"
-			}')
-
+			$token = [Mc2it.Agicap.Authentication.AccessToken] (ConvertFrom-Json (Get-Content "$PSScriptRoot/../../Resources/Authentication/AccessToken.json" -Raw))
 			Should-BeFalse $token.HasExpired
 			Should-BeCollection "agicap:public-api", "public-api:import_payment_files", "public-api:manage-payment-beneficiaries" $token.Scopes
 			Should-Be ([WebAuthenticationType]::OAuth) $token.Type
