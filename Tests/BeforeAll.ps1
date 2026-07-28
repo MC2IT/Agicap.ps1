3f5952d9b84549454f030b5f52f1f691f7c9fdc9
Cédric Belin <cedx@outlook.com>
2026-07-28 18:48:32 +0200

    Fix the CI

--- Tests/BeforeAll.ps1
+++ Tests/BeforeAll.ps1
@@ -2,8 +2,9 @@ using namespace System.Diagnostics.CodeAnalysis
 using module ../Agicap.psd1
 
 # The client identifier and secret.
-$credential = [pscredential]::new($Env:AGICAP_CLIENT_ID, (ConvertTo-SecureString $Env:AGICAP_CLIENT_SECRET -AsPlainText))
+$clientId = $Env:AGICAP_CLIENT_ID ?? "FooBar"
+$clientSecret = ConvertTo-SecureString ($Env:AGICAP_CLIENT_SECRET ?? "BazQux") -AsPlainText
 
 # The client used to query the Agicap API.
 [SuppressMessage("PSUseDeclaredVarsMoreThanAssignments", "client")]
-$client = New-AgicapClient $credential
+$client = New-AgicapClient ([pscredential]::new($clientId, $clientSecret))
