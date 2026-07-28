57d4e3600b32a73532b3c9562ce6b1024b12611a
Cédric Belin <cedx@outlook.com>
2026-07-28 19:39:53 +0200

    Add the `Select-OrganizationEntity` cmdlet

---
+++ Sources/Organizations/Select-OrganizationEntity.ps1
@@ -0,0 +1,31 @@
+﻿using namespace Mc2it.Agicap
+
+<#
+.SYNOPSIS
+	Fetches the entities of the organization with the specified identifier.
+.OUTPUTS
+	The entities of the organization with the specified identifier.
+#>
+function Select-OrganizationEntity {
+	[CmdletBinding()]
+	[OutputType([Mc2it.Agicap.PaginatedList[Mc2it.Agicap.Organizations.Entity]])]
+	param (
+		# The API client.
+		[Parameter(Mandatory, Position = 1)]
+		[Client] $Client,
+
+		# The organization identifier.
+		[Parameter(Mandatory, Position = 2)]
+		[guid] $Organization,
+
+		# The page number.
+		[ValidateRange("Positive")]
+		[int] $PageNumber = 1,
+
+		# The number of elements per page.
+		[ValidateRange("Positive")]
+		[int] $PageSize = 100
+	)
+
+	$Client.Organizations.GetEntities($Organization, $PageNumber, $PageSize)
+}
