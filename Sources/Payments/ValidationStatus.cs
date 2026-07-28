505b57a3ada5b5819f2f08bb36382ed9a1e1c7ae
Cédric Belin <cedx@outlook.com>
2026-07-28 18:20:34 +0200

    Remove the models already provided by the .NET assembly

--- Sources/Payments/ValidationStatus.cs
+++
@@ -1,9 +0,0 @@
-namespace Mc2it.Agicap.Payments;
-
-/// <summary>
-/// Defines the validation status of a beneficiary.
-/// </summary>
-public enum ValidationStatus {
-	PendingValidation,
-	Validated
-}
