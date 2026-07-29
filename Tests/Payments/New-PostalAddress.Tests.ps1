67c732d764e3b2dff467e809dbd71bc264e1d300
Cédric Belin <cedx@outlook.com>
2026-07-29 14:45:05 +0200

    Add tests for the `New-PostalAddress` cmdlet

---
+++ Tests/Payments/New-PostalAddress.Tests.ps1
@@ -0,0 +1,19 @@
+﻿<#
+.SYNOPSIS
+	Tests the features of the `New-PostalAddress` cmdlet.
+#>
+Describe "New-PostalAddress" {
+	BeforeAll { . "$PSScriptRoot/../BeforeAll.ps1" }
+
+	It "should return a new postal address" {
+		$postalAddress = New-AgicapPostalAddress -City " " -Country " " -StreetName " "
+		Should-BeTrue $postalAddress.IsEmpty
+		Should-BeNull $postalAddress.Number
+		Should-BeNull $postalAddress.ZipCode
+
+		$postalAddress = New-AgicapPostalAddress -City "Paris" -Country "FR" -StreetName "Rue de la Paix"
+		Should-BeFalse $postalAddress.IsEmpty
+		Should-BeString "Paris" $postalAddress.City -CaseSensitive
+		Should-BeString "Rue de la Paix" $postalAddress.StreetName -CaseSensitive
+	}
+}
