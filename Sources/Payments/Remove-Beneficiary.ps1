4404061f0e5cb31b62e0167949d1e2f9835eb4a7
Cédric Belin <cedx@outlook.com>
2026-07-29 16:22:40 +0200

    Update the VSCode settings [skip ci]

---
+++ Sources/Payments/Remove-Beneficiary.ps1
@@ -0,0 +1,31 @@
+﻿using namespace Mc2it.Agicap.Payments
+using namespace System.Diagnostics.CodeAnalysis
+
+<#
+.SYNOPSIS
+	Creates a new beneficiary.
+.OUTPUTS
+	The newly created beneficiary.
+#>
+# function Remove-Beneficiary {
+# 	[CmdletBinding()]
+# 	[OutputType([Mc2it.Agicap.Payments.Beneficiary])]
+# 	[SuppressMessage("PSUseShouldProcessForStateChangingFunctions", "")]
+# 	param (
+# 		# The name of the beneficiary.
+# 		[Parameter(Mandatory, Position = 1)]
+# 		[string] $Name,
+
+# 		# The bank account of the beneficiary.
+# 		[BankAccount] $BankAccount,
+
+# 		# The postal address of the beneficiary.
+# 		[switch] $All
+# 	)
+
+# 	return [Beneficiary]@{
+# 		BankAccount = $BankAccount
+# 		Name = $Name
+# 		PostalAddress = $PostalAddress
+# 	}
+# }
