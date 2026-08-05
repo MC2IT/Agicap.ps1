1f66a2c4e2ef74efe30acab4f78c3ae0bd3a0f6f
Cédric Belin <cedx@outlook.com>
2026-08-05 17:25:51 +0200

    Add the `Remove-ThirdParty` cmdlet [skip ci]

--- Agicap.psd1
+++ Agicap.psd1
@@ -24,6 +24,7 @@
 		"New-SynchronizedBeneficiary"
 		"Remove-AccountingAccount"
 		"Remove-Beneficiary"
+		"Remove-ThirdParty"
 		"Request-AccessToken"
 		"Select-Beneficiary"
 		"Select-Entity"
---
+++ Sources/ChartOfAccounts/Remove-ThirdParty.ps1
@@ -0,0 +1,38 @@
+﻿using namespace Mc2it.Agicap
+using namespace Mc2it.Agicap.Payments
+using namespace System.Diagnostics.CodeAnalysis
+using namespace System.Net.Http
+
+<#
+.SYNOPSIS
+	Deletes the third-parties with the specified codes.
+.INPUTS
+	The codes of third-parties to delete.
+#>
+function Remove-ThirdParty {
+	[CmdletBinding()]
+	[OutputType([void])]
+	[SuppressMessage("PSUseShouldProcessForStateChangingFunctions", "")]
+	param (
+		# The API client.
+		[Parameter(Mandatory, Position = 1)]
+		[Client] $Client,
+
+		# The entity identifier.
+		[Parameter(Mandatory, Position = 2)]
+		[int] $EntityId,
+
+		# The codes of third-parties to delete.
+		[Parameter(Mandatory, Position = 3, ValueFromPipeline)]
+		[string[]] $InputObject
+	)
+
+	begin {
+		$api = $Client.ChartOfAccounts.ThirdParties($EntityId)
+	}
+
+	process {
+		try { $api.Delete($InputObject) }
+		catch [HttpRequestException] { Write-Error $_ }
+	}
+}
