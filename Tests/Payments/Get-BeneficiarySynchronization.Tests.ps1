cfac740e197f8f0283ed900a15417b43278f91e5
Cédric Belin <cedx@outlook.com>
2026-08-06 16:40:46 +0200

    Code formatting

--- Tests/Payments/Get-BeneficiarySynchronization.Tests.ps1
+++ Tests/Payments/Get-BeneficiarySynchronization.Tests.ps1
@@ -6,7 +6,7 @@ Describe "Get-BeneficiarySynchronization" -Skip:($Env:CI -eq "true") {
 	BeforeAll { . "$PSScriptRoot/../BeforeAll.ps1" }
 
 	It "should return a beneficiary synchronization report" {
-		$syncId = "3c648676-e07e-4aca-8e63-ce0802221b57" | New-Guid
+		$syncId = New-Guid "3c648676-e07e-4aca-8e63-ce0802221b57"
 
 		$synchronization = Get-AgicapBeneficiarySynchronization $client $entityId $syncId
 		Should-Be (Get-Date "2026-08-03T08:21:47.346206+00:00").Date $synchronization.CreatedAt.Date
