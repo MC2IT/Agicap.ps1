990b9c5d12d2c93a3f790c216ca77c9f4608b67f
Cédric Belin <cedx@outlook.com>
2026-08-12 15:46:17 +0200

    Add tests for the `Select-AccountingPurchase` cmdlet

---
+++ Tests/PurchaseJournal/Select-AccountingPurchase.Tests.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.SYNOPSIS
+	Tests the features of the `Select-AccountingPurchase` cmdlet.
+#>
+Describe "Select-AccountingPurchase" -Skip:($Env:CI -eq "true") {
+	BeforeAll { . "$PSScriptRoot/../BeforeAll.ps1" }
+
+	It "should return the entries of the purchase journal" {
+		$lastSynchronizationDate = [datetime] "2026-01-01T00:00:00Z"
+		$purchaseJournalEntries = (Select-AgicapAccountingPurchase $client $entityId $lastSynchronizationDate -PageSize 2).Items
+		Should-BeCollection $purchaseJournalEntries -Count 2
+
+		foreach ($purchaseJournalEntry in $purchaseJournalEntries) {
+			Should-NotBe ([guid]::Empty) $purchaseJournalEntry.AgicapUniqueId
+			Should-BeGreaterThanOrEqual 1 $purchaseJournalEntry.AccountingLines.Count
+			Should-Be 0 ($purchaseJournalEntry.AccountingLines | Measure-Object -Sum { $_.Credit - $_.Debit }).Sum
+			Should-Be $purchaseJournalEntry.AccountingLines.Count $purchaseJournalEntry.AccountingLines.Where{ $_.Currency -eq "EUR" }.Count
+		}
+	}
+}
