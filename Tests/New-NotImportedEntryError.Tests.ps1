08743183a8f79e676464c58ce4fa980a974adcae
Cédric Belin <cedx@outlook.com>
2026-08-12 10:58:48 +0200

    Add tests for the `New-NotImportedEntryError` cmdlet

---
+++ Tests/New-NotImportedEntryError.Tests.ps1
@@ -0,0 +1,17 @@
+﻿<#
+.SYNOPSIS
+	Tests the features of the `New-NotImportedEntryError` cmdlet.
+#>
+Describe "New-NotImportedEntryError" {
+	BeforeAll { . "$PSScriptRoot/BeforeAll.ps1" }
+
+	It "should return a new entry error" {
+		$notImportedEntryError = New-AgicapNotImportedEntryError UNKNOWN_CURRENCY
+		Should-BeNull $notImportedEntryError.ErrorMessage
+		Should-BeString ([Mc2it.Agicap.NotImportedEntryErrorTypes]::UnknownCurrency) $notImportedEntryError.ErrorType -CaseSensitive
+
+		$notImportedEntryError = New-AgicapNotImportedEntryError UNKNOWN_THIRD_PARTY "An error occurred."
+		Should-BeString "An error occurred." $notImportedEntryError.ErrorMessage -CaseSensitive
+		Should-BeString ([Mc2it.Agicap.NotImportedEntryErrorTypes]::UnknownThirdParty) $notImportedEntryError.ErrorType -CaseSensitive
+	}
+}
