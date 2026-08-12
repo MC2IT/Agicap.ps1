f4eed7b6664930a677e62b83aac3022cea83a44c
Cédric Belin <cedx@outlook.com>
2026-08-12 11:45:52 +0200

    Add tests for the `New-NotImportedEntry` cmdlet

---
+++ Tests/New-NotImportedEntry.Tests.ps1
@@ -0,0 +1,16 @@
+﻿<#
+.SYNOPSIS
+	Tests the features of the `New-NotImportedEntry` cmdlet.
+#>
+Describe "New-NotImportedEntry" {
+	BeforeAll { . "$PSScriptRoot/BeforeAll.ps1" }
+
+	It "should return a new entry" {
+		$guid = New-Guid
+		$importErrror = New-AgicapNotImportedEntryError UNKNOWN_VAT_ACCOUNT "An error occurred."
+
+		$notImportedEntry = New-AgicapNotImportedEntry $guid $importErrror
+		Should-Be $guid $notImportedEntry.EntryAgicapUniqueId
+		Should-BeCollection @($importErrror) $notImportedEntry.Errors
+	}
+}
