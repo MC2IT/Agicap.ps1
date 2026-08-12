ba0fafcafce2744feea6f937766878f3859ee9c3
Cédric Belin <cedx@outlook.com>
2026-08-12 15:37:15 +0200

    Add tests for the `Deny-AccountingPurchase` cmdlet

---
+++ Tests/PurchaseJournal/Deny-AccountingPurchase.Tests.ps1
@@ -0,0 +1,11 @@
+﻿<#
+.SYNOPSIS
+	Tests the features of the `Deny-AccountingPurchase` cmdlet.
+#>
+Describe "Deny-AccountingPurchase" -Skip:($Env:CI -eq "true") {
+	BeforeAll { . "$PSScriptRoot/../BeforeAll.ps1" }
+
+	It "should report errors on exported purchase journal entries" -Skip {
+		# This test requires an Agicap development environment.
+	}
+}
