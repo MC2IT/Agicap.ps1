51e6fadd5d74df1fc1ec10716e11af55ed8a994b
Cédric Belin <cedx@outlook.com>
2026-08-29 01:27:06 +0200

    Module merging

---
+++ Sources/Payments/Beneficiary.ps1
@@ -0,0 +1,211 @@
+﻿using namespace Mc2it.Agicap
+using namespace Mc2it.Agicap.Payments
+using namespace System.Diagnostics.CodeAnalysis
+using namespace System.Net.Http
+
+<#
+.SYNOPSIS
+	Creates a new beneficiary.
+.OUTPUTS
+	The newly created beneficiary.
+#>
+function New-Beneficiary {
+	[CmdletBinding()]
+	[OutputType([Mc2it.Agicap.Payments.Beneficiary])]
+	[SuppressMessage("PSUseShouldProcessForStateChangingFunctions", "")]
+	param (
+		# The name of the beneficiary.
+		[Parameter(Mandatory, Position = 1)]
+		[string] $Name,
+
+		# The bank account of the beneficiary.
+		[BankAccount] $BankAccount,
+
+		# The postal address of the beneficiary.
+		[PostalAddress] $PostalAddress
+	)
+
+	return [Beneficiary]@{
+		BankAccount = $BankAccount
+		Name = $Name
+		PostalAddress = $PostalAddress
+	}
+}
+
+<#
+.SYNOPSIS
+	Deletes either the specified beneficiary, or all beneficiaries.
+.INPUTS
+	The beneficiary to delete.
+#>
+function Remove-Beneficiary {
+	[CmdletBinding(DefaultParameterSetName = "InputObject")]
+	[OutputType([void])]
+	[SuppressMessage("PSUseShouldProcessForStateChangingFunctions", "")]
+	param (
+		# The API client.
+		[Parameter(Mandatory, Position = 1)]
+		[Client] $Client,
+
+		# The entity identifier.
+		[Parameter(Mandatory, Position = 2)]
+		[int] $EntityId,
+
+		# The beneficiary to delete.
+		[Parameter(Mandatory, ParameterSetName = "InputObject", Position = 3, ValueFromPipeline)]
+		[Beneficiary] $InputObject,
+
+		# The identifier of the beneficiary to delete.
+		[Parameter(Mandatory, ParameterSetName = "BeneficiaryId", Position = 3)]
+		[guid] $BeneficiaryId,
+
+		# Value indicating whether to delete all beneficiaries.
+		[Parameter(ParameterSetName = "All")]
+		[switch] $All
+	)
+
+	begin {
+		$api = $Client.Payments.Beneficiaries($EntityId)
+	}
+
+	process {
+		try {
+			if ($All) { $api.DeleteAll() }
+			else { $api.Delete($InputObject ?? $BeneficiaryId) }
+		}
+		catch [HttpRequestException] {
+			Write-Error $_
+		}
+	}
+}
+
+<#
+.SYNOPSIS
+	Fetches the beneficiaries of the entity with the specified identifier.
+.OUTPUTS
+	The beneficiaries of the entity with the specified identifier.
+#>
+function Select-Beneficiary {
+	[CmdletBinding()]
+	[OutputType([Mc2it.Agicap.Payments.Beneficiary])]
+	param (
+		# The API client.
+		[Parameter(Mandatory, Position = 1)]
+		[Client] $Client,
+
+		# The entity identifier.
+		[Parameter(Mandatory, Position = 2)]
+		[int] $EntityId
+	)
+
+	begin {
+		$api = $Client.Payments.Beneficiaries($EntityId)
+	}
+
+	end {
+		try { $api.ReadAll() }
+		catch [HttpRequestException] { Write-Error $_ }
+	}
+}
+
+<#
+.SYNOPSIS
+	Creates a new beneficiary.
+.INPUTS
+	The beneficiary to create.
+.OUTPUTS
+	The identifier of the newly created beneficiary.
+#>
+function Submit-Beneficiary {
+	[CmdletBinding()]
+	[OutputType([guid])]
+	param (
+		# The API client.
+		[Parameter(Mandatory, Position = 1)]
+		[Client] $Client,
+
+		# The entity identifier.
+		[Parameter(Mandatory, Position = 2)]
+		[int] $EntityId,
+
+		# The beneficiary to create.
+		[Parameter(Mandatory, Position = 3, ValueFromPipeline)]
+		[Beneficiary] $InputObject
+	)
+
+	begin {
+		$api = $Client.Payments.Beneficiaries($EntityId)
+	}
+
+	process {
+		try { $api.Create($InputObject) }
+		catch [HttpRequestException] { Write-Error $_ }
+	}
+}
+
+<#
+.SYNOPSIS
+	Starts a bulk synchronization of beneficiaries from the ERP software.
+.OUTPUTS
+	The identifier of the newly started synchronization.
+#>
+function Sync-Beneficiary {
+	[CmdletBinding()]
+	[OutputType([guid])]
+	param (
+		# The API client.
+		[Parameter(Mandatory, Position = 1)]
+		[Client] $Client,
+
+		# The entity identifier.
+		[Parameter(Mandatory, Position = 2)]
+		[int] $EntityId,
+
+		# The beneficiaries to synchronize.
+		[Parameter(Mandatory, Position = 3)]
+		[SynchronizedBeneficiary[]] $Beneficiary
+	)
+
+	begin {
+		$api = $Client.Payments.Beneficiaries($EntityId).Synchronization
+	}
+
+	end {
+		try { $api.Create($Beneficiary) }
+		catch [HttpRequestException] { Write-Error $_ }
+	}
+}
+
+<#
+.SYNOPSIS
+	Updates the specified beneficiary.
+.INPUTS
+	The beneficiary to update.
+#>
+function Update-Beneficiary {
+	[CmdletBinding()]
+	[OutputType([void])]
+	[SuppressMessage("PSUseShouldProcessForStateChangingFunctions", "")]
+	param (
+		# The API client.
+		[Parameter(Mandatory, Position = 1)]
+		[Client] $Client,
+
+		# The entity identifier.
+		[Parameter(Mandatory, Position = 2)]
+		[int] $EntityId,
+
+		# The beneficiary to update.
+		[Parameter(Mandatory, Position = 3, ValueFromPipeline)]
+		[Beneficiary] $InputObject
+	)
+
+	begin {
+		$api = $Client.Payments.Beneficiaries($EntityId)
+	}
+
+	process {
+		try { $api.Update($InputObject) }
+		catch [HttpRequestException] { Write-Error $_ }
+	}
+}
---
+++ Tests/Payments/Beneficiary.Tests.ps1
@@ -0,0 +1,91 @@
+﻿using namespace System.Diagnostics.CodeAnalysis
+using namespace System.Net
+
+<#
+.SYNOPSIS
+	Tests the features of the `New-Beneficiary` cmdlet.
+#>
+Describe "New-Beneficiary" {
+	BeforeAll { . "$PSScriptRoot/../BeforeAll.ps1" }
+
+	It "should return a new beneficiary" {
+		$postalAddress = New-AgicapPostalAddress -City " " -Country " " -StreetName " "
+		$beneficiary = New-AgicapBeneficiary "My Company" -PostalAddress $postalAddress
+		Should-BeString "My Company" $beneficiary.Name -CaseSensitive
+		Should-BeNull $beneficiary.BankAccount
+		Should-Be (New-Guid -Empty) $beneficiary.Id
+		Should-BeNull $beneficiary.PostalAddress
+	}
+}
+
+<#
+.SYNOPSIS
+	Tests the features of the `Select-Beneficiary` cmdlet.
+#>
+Describe "Select-Beneficiary" -Skip:($Env:CI -eq "true") {
+	BeforeAll { . "$PSScriptRoot/../BeforeAll.ps1" }
+
+	It "should return the beneficiaries of the entity with the specified identifier" {
+		$list = Select-AgicapBeneficiary $client $entityId
+		Should-BeGreaterThan 1 $list.Count
+
+		$beneficiary = $list.Where{ $_.Name -like "Agicap*" }
+		Should-NotBeNull $beneficiary.PostalAddress
+		Should-BeString Lyon $beneficiary.PostalAddress.City
+		Should-BeString FR $beneficiary.PostalAddress.Country -CaseSensitive
+	}
+}
+
+<#
+.SYNOPSIS
+	Tests the features of `Submit-Beneficiary`, `Update-Beneficiary` and `Remove-Beneficiary` cmdlets.
+#>
+Describe "Submit-Beneficiary" -Skip:($Env:CI -eq "true") {
+	BeforeAll {
+		. "$PSScriptRoot/../BeforeAll.ps1"
+		$postalAddress = New-AgicapPostalAddress -City "Fabrègues" -Country FR -StreetName "Rue Gine"
+		[SuppressMessage("PSUseDeclaredVarsMoreThanAssignments", "beneficiary")]
+		$beneficiary = New-AgicapBeneficiary "MC2IT Test Runner" -PostalAddress $postalAddress
+	}
+
+	It "should create the specified beneficiary" {
+		Should-Be (New-Guid -Empty) $beneficiary.Id
+		$beneficiary | Submit-AgicapBeneficiary $client $entityId -ErrorAction Stop | Out-Null
+		Should-NotBe (New-Guid -Empty) $beneficiary.Id
+	}
+
+	It "should throw an exception if the beneficiary already exists" {
+		try {
+			$beneficiary | Submit-AgicapBeneficiary $client $entityId -ErrorAction Stop | Out-Null
+			throw "The exception was not thrown as planned."
+		}
+		catch [Mc2it.Agicap.HttpResponseException] {
+			Should-Be ([HttpStatusCode]::Conflict) $_.Exception.StatusCode
+			Should-BeLikeString "*beneficiary*MC2IT*exists*" $_.Exception.ProblemDetails?.Title
+		}
+	}
+
+	It "should update the specified beneficiary" {
+		$beneficiary.PostalAddress.Number = "29"
+		$beneficiary.PostalAddress.ZipCode = "34690"
+		$beneficiary | Update-AgicapBeneficiary $client $entityId -ErrorAction Stop
+	}
+
+	It "should delete the specified beneficiary" {
+		$beneficiary | Remove-AgicapBeneficiary $client $entityId -ErrorAction Stop
+	}
+}
+
+<#
+.SYNOPSIS
+	Tests the features of the `Sync-Beneficiary` cmdlet.
+#>
+Describe "Sync-Beneficiary" -Skip:($Env:CI -eq "true") {
+	BeforeAll { . "$PSScriptRoot/../BeforeAll.ps1" }
+
+	It "should synchronize the specified beneficiary" {
+		$postalAddress = New-AgicapPostalAddress -City "Fabrègues" -Country FR -StreetName "Rue Gine"
+		$beneficiary = New-AgicapSynchronizedBeneficiary "MC2IT-DEVELOPMENT" (New-AgicapBeneficiary "MC2IT Development Department" -PostalAddress $postalAddress)
+		Should-NotBe (New-Guid -Empty) (Sync-AgicapBeneficiary $client $entityId $beneficiary)
+	}
+}
