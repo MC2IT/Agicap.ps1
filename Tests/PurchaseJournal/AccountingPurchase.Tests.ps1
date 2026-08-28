1b1106257b0b73455cd2170a807cec3b582ec07a
Cédric Belin <cedx@outlook.com>
2026-08-29 01:20:20 +0200

    Module merging

--- Tests/PurchaseJournal/AccountingPurchase.Tests.ps1
+++ Tests/PurchaseJournal/AccountingPurchase.Tests.ps1
@@ -1,5 +1,17 @@
 ﻿<#
 .SYNOPSIS
+	Tests the features of the `Deny-AccountingPurchase` cmdlet.
+#>
+Describe "Deny-AccountingPurchase" -Skip:($Env:CI -eq "true") {
+	BeforeAll { . "$PSScriptRoot/../BeforeAll.ps1" }
+
+	It "should notify Agicap that the specified purchase journal entries were not correctly imported" {
+		Set-ItResult -Skipped -Because "This test requires an Agicap development environment."
+	}
+}
+
+<#
+.SYNOPSIS
 	Tests the features of the `Select-AccountingPurchase` cmdlet.
 #>
 Describe "Select-AccountingPurchase" -Skip:($Env:CI -eq "true") {
