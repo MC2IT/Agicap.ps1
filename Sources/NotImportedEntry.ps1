9ed3a5926e9577bb7e7351c4afe78c643339a351
Cédric Belin <cedx@outlook.com>
2026-08-29 01:13:13 +0200

    Module merging

--- Sources/NotImportedEntry.ps1
+++ Sources/NotImportedEntry.ps1
@@ -3,6 +3,32 @@ using namespace System.Diagnostics.CodeAnalysis
 
 <#
 .SYNOPSIS
+	Creates a new purchase journal entry that was not imported in the client accounting system.
+.OUTPUTS
+	The newly created entry.
+#>
+function New-NotImportedEntry {
+	[CmdletBinding()]
+	[OutputType([Mc2it.Agicap.NotImportedEntry])]
+	[SuppressMessage("PSUseShouldProcessForStateChangingFunctions", "")]
+	param (
+		# A unique identifier from Agicap.
+		[Parameter(Mandatory, Position = 1)]
+		[guid] $EntryAgicapUniqueId,
+
+		# The errors preventing the journal entry from being imported.
+		[Parameter(Mandatory, Position = 2)]
+		[NotImportedEntryError[]] $Errors
+	)
+
+	return [NotImportedEntry]@{
+		EntryAgicapUniqueId = $EntryAgicapUniqueId
+		Errors = $Errors
+	}
+}
+
+<#
+.SYNOPSIS
 	Creates a new import error for a purchase journal entry.
 .OUTPUTS
 	The newly created entry error.
--- Tests/NotImportedEntry.Tests.ps1
+++ Tests/NotImportedEntry.Tests.ps1
@@ -1,5 +1,22 @@
 ﻿<#
 .SYNOPSIS
+	Tests the features of the `New-NotImportedEntry` cmdlet.
+#>
+Describe "New-NotImportedEntry" {
+	BeforeAll { . "$PSScriptRoot/BeforeAll.ps1" }
+
+	It "should return a new entry marked as not imported" {
+		$guid = New-Guid
+		$notImportedEntryError = New-AgicapNotImportedEntryError UNKNOWN_VAT_ACCOUNT "An error occurred."
+
+		$notImportedEntry = New-AgicapNotImportedEntry $guid $notImportedEntryError
+		Should-Be $guid $notImportedEntry.EntryAgicapUniqueId
+		Should-BeCollection @($notImportedEntryError) $notImportedEntry.Errors
+	}
+}
+
+<#
+.SYNOPSIS
 	Tests the features of the `New-NotImportedEntryError` cmdlet.
 #>
 Describe "New-NotImportedEntryError" {
